c84d131f758d6a99999239d1c42a0f6c4ca1d048
sameer reza khan <reza_sameer200@yahoo.com>
2026-04-17 07:38:49 +0530

    updated notification schema

--- src/main/resources/db/changelog/post-bootstrap/017_update_notification_schema.sql
+++ src/main/resources/db/changelog/post-bootstrap/017_update_notification_schema.sql
@@ -1,6 +1,6 @@
 -- liquibase formatted sql
 
--- changeset sameerkhan:add_missing_notification_columns
+-- changeset sameerkhan:add_missing_notification_columns_v2
 ALTER TABLE "notification"
 ADD COLUMN IF NOT EXISTS "notification_id" VARCHAR(255),
 ADD COLUMN IF NOT EXISTS "distributor_id" BIGINT,
@@ -10,12 +10,19 @@ ADD COLUMN IF NOT EXISTS "title" VARCHAR(255),
 ADD COLUMN IF NOT EXISTS "content" VARCHAR(2000),
 ADD COLUMN IF NOT EXISTS "recipient" VARCHAR(255),
 ADD COLUMN IF NOT EXISTS "sender" VARCHAR(255),
+ADD COLUMN IF NOT EXISTS "scheduled_for" TIMESTAMP WITH TIME ZONE,
 ADD COLUMN IF NOT EXISTS "retry_count" INTEGER DEFAULT 0,
 ADD COLUMN IF NOT EXISTS "error_message" TEXT;
 
 -- If notification_id should be unique and non-null as per your entity:
 UPDATE "notification" SET "notification_id" = id::text WHERE "notification_id" IS NULL;
-ALTER TABLE "notification" ALTER COLUMN "notification_id" SET NOT NULL;
+
+DO $$
+BEGIN
+    IF EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name = 'notification' AND column_name = 'notification_id') THEN
+        ALTER TABLE "notification" ALTER COLUMN "notification_id" SET NOT NULL;
+    END IF;
+END $$;
 
 DO $$
 BEGIN
